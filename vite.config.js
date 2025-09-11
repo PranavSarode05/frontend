d3fc000c2967d21a60ad43e5728c02cc95dfb212
PranavSarode05 <pranavsarode05@gmail.com>
2025-09-11 14:59:42 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  base: './',
+  css: {
+    preprocessorOptions: {
+      css: {
+        charset: false
+      }
+    }
+  }
 })
